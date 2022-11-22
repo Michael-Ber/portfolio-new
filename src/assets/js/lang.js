70dd8ab66cfaddfa2a16c3c29bf711708694215e
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2022-11-22 16:47:04 +0800

    headers animation done

--- src/assets/js/lang.js
+++ src/assets/js/lang.js
@@ -1,32 +1,43 @@
+'use strict';
+
+import Typed from 'typed.js';
 
 const lang = {
     'lng-about' : {
-        'en' : 'about',
-        'ru': 'обо мне'
+        'en' : 'About',
+        'ru': 'Обо мне'
     },
     'lng-skills' : {
-        'en' : 'skills',
-        'ru': 'навыки'
+        'en' : 'Skills',
+        'ru': 'Навыки'
     },
     'lng-works' : {
-        'en' : 'works',
-        'ru': 'мои работы'
+        'en' : 'Works',
+        'ru': 'Мои работы'
     },
     'lng-contact' : {
-        'en' : 'contact',
-        'ru': 'контакты'
+        'en' : 'Contact',
+        'ru': 'Контакты'
     },
     'lng-intro' : {
-        'en' : `Hi, <br>I'm Michael,<br> web developer`,
-        'ru': 'Привет,<br> меня зовут Михаил,<br> я веб-разработчик'
+        'en' : [`Hi,`, `I'm Michael,`, `web developer`],
+        'ru': ['Привет,', 'меня зовут Михаил,', 'я веб-разработчик']
     },
+    // 'lng-intro-2' : {
+    //     'en' : `I'm Michael,`,
+    //     'ru': 'меня зовут Михаил,'
+    // },
+    // 'lng-intro-3' : {
+    //     'en' : `web developer`,
+    //     'ru': 'я веб-разработчик'
+    // },
     'lng-btnContact' : {
         'en' : 'Contact me!',
         'ru': 'Свяжитесь со мной'
     },
     'lng-prof' : {
-        'en' : 'Freelancer',
-        'ru': 'Фрилансер'
+        'en' : ['Freelancer', 'Front-end developer'],
+        'ru': ['Фрилансер', 'Фронт-енд разработчик']
     },
     'lng-descr' : {
         'en' : "Hello, my name is Michael. I'm junior front-end developer.I've been learning front-end for two years, finished five web developing courses on Udemy and have been practicing all that time. My you can see below.",
@@ -125,24 +136,95 @@ const lang = {
 function setLanguage(langObject, langSelector) {
     const select = document.querySelector(langSelector);
     const tagsForLangChange =  document.querySelectorAll('.lng');
-    window.location.hash = '#en';
-    select.addEventListener('change', () => {
-        let language = select.value;
-        
-        window.location.hash  = `#${language}`;
-        
-        let hash = window.location.hash;
+    const introTitle = Array.from(document.querySelector('.intro__title').children);
+    const aboutTitle = document.querySelector('.about__subtitle');
 
+    window.location.hash = '#en';
+    let hashOld = window.location.hash.substring(1);
+    let typed1 = [];
+
+    // Typed.js for intro__title, no loop
+    
+    for(let i = 0; i < introTitle.length; i++) {
+        const typed = new Typed(introTitle[i], {
+            strings: [lang['lng-intro'][window.location.hash.substring(1)][i]],
+            typeSpeed: 40,
+            startDelay: 900 * i,
+            showCursor: false,
+            onReset: self => {
+                self.strings = [lang['lng-intro'][window.location.hash.substring(1)][i]]
+            },
+            onComplete: self => {
+                animateText(introTitle[i], 'title-intro__letter');
+            }
+        });
+        typed1.push(typed);
+    }
+    //
+
+    // Typed.js for about__subtitle with loop=true
+    let typed2 = new Typed(aboutTitle, {
+        strings: lang['lng-prof'][window.location.hash.substring(1)],
+        typeSpeed: 60,
+        loop: true,
+        showCursor: false,
+        smartBackspace: false,
+        startDelay: 500,
+        backDelay: 1000,
+        backSpeed: 30,
+        onReset: (self) => {
+            aboutTitle.style.opacity = 0;
+        },
+        onBegin: (self) => {
+            setTimeout(() => {
+                aboutTitle.style.opacity = 1;
+            }, 600);
+            self.strings = lang['lng-prof'][window.location.hash.substring(1)];
+            self.strings.options = lang['lng-prof'][window.location.hash.substring(1)];
+        }
+    });
+    //
+    select.addEventListener('input', () => {
+        let language = select.value;
+        window.location.hash  = `#${language}`;
+        let hash = window.location.hash.substring(1);
+        
+        if(hashOld !== hash) {
+            hashOld = hash;
+            typed1.forEach((obj, j) => {
+                obj.reset();
+            });
+            typed2.reset();
+        }
+        
         tagsForLangChange.forEach(item => {
             const clsNameStr = item.classList.value.match(/lng-[a-z]*/ig).join('');
-            console.log(item);
             if(item.classList.contains(clsNameStr)) {
-                item.innerHTML = langObject[clsNameStr][hash.substring(1)];
+                item.innerHTML = langObject[clsNameStr][hash];
             }
-            // console.log(clsNameStr);
-            // console.log(lang[clsNameStr]['ru']);
         });
     })
+
+    //Animate literals
+    function animateText(parentNode, letterClass) {
+        let parent = parentNode;
+        let content = parent.innerHTML;
+        parent.innerHTML = '';
+
+        for(let i = 0; i < content.length; i++) {
+            let letter = document.createElement('span');
+            if(content[i] == ' ') {
+                letter.classList.add('space-item');
+                letter.innerHTML = content[i];
+                parent.appendChild(letter);
+            }else {
+                letter.classList.add(letterClass);
+                letter.innerHTML = content[i];
+                parent.appendChild(letter);
+            }
+        }
+    }
+
 }
 
 export {setLanguage, lang};
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
 'use strict';
-
 import {burger} from './burger';
 import {scroll} from './scroll';
 import {setLanguage, lang} from './lang';
+
 window.addEventListener('DOMContentLoaded', () => {
     burger({
         btn: '.burger__btn',
@@ -15,10 +15,9 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     scroll({arrow: '.arrow-up', arrowActive: 'arrow-up_active'});
 
-    document.querySelectorAll('.lng').forEach(item => {
-        const clsNameStr = item.classList.value.match(/lng-[a-z]*/ig).join('');
-        // console.log(clsNameStr);
-        // console.log(lang[clsNameStr]['ru']);
-    });
     setLanguage(lang, '.lang-selector');
+    
+    
+   
+   
 });
