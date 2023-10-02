17f86e5035dd668efaea185d7ca067ddab05b112
Michael-Ber <mikeber000@gmail.com>
2023-10-02 20:57:26 +0800

    nav as slider, remove twitching from scroll bar

--- src/assets/js/section-commutator.js
+++ src/assets/js/section-commutator.js
@@ -1,19 +1,49 @@
+
+
+
 export const sectionCommutator = () => {
+    // const sections = document.querySelectorAll('section');
+    // const linksWrapper = document.querySelector('.links-header__list');
+
+    // linksWrapper.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     if (e.target.hash) {
+    //         const showSection = Array.from(sections).filter(section => section.id === e.target.hash.substring(1))[0];
+    //         sections.forEach(section => section.classList.remove('section-active'));
+    //         if (e.target.hash === '#about') {
+    //             showSection.style.display = 'flex';
+    //             showSection.classList.add('section-active');
+    //         } else {
+    //             showSection.style.display = 'block';
+    //         }
+
+    //     }
+    // })
+
+    const slider = document.querySelector('.slider');
+    const sliderWrapper = document.querySelector('.slider-wrapper');
     const sections = document.querySelectorAll('section');
     const linksWrapper = document.querySelector('.links-header__list');
+    const slideWidth = slider.getBoundingClientRect().width;
+    const slideHeight= slider.getBoundingClientRect().height;
+    const toFirstSlide = document.querySelector('.header__logo');
 
+    sliderWrapper.style.width = slideWidth * sections.length + 'px';
+    sections.forEach(section => section.style.width);
+    slider.style.height = '100vh';
+    console.log(window.innerHeight);
     linksWrapper.addEventListener('click', (e) => {
         e.preventDefault();
         if (e.target.hash) {
-            const showSection = Array.from(sections).filter(section => section.id === e.target.hash.substring(1))[0];
-            sections.forEach(section => section.style.display = 'none');
-            if (e.target.hash === '#about') {
-                showSection.style.display = 'flex';
-            } else {
-                showSection.style.display = 'block';
-            }
-
+            const sectionNum = Array.from(sections).findIndex(section => section.id === e.target.hash.substring(1));
+            sliderWrapper.style.transform = `translateX(-${sectionNum * slideWidth}px)`;
+            slider.style.height = Array.from(sections)[sectionNum].getBoundingClientRect().height + 'px';
         }
-    })
+    });
+    toFirstSlide.addEventListener('click', (e) => {
+        e.preventDefault();
+        sliderWrapper.style.transform = `translateX(${0}px)`;
+    });
+
 
 }
